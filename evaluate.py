23ed7b1039d71946657176627a4d81c7e4274047
Nguyen Hoang Son <sonnguyen9616@gmail.com>
2024-09-26 00:24:21 +0700

    For low-spec laptop

---
+++ evaluate.py
@@ -0,0 +1,28 @@
+import time
+from argparse import ArgumentParser
+
+from src.compute import compute_mAP
+
+ACCEPTED_IMAGE_EXTS = ['.jpg', '.png']
+
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("--feature_extractor", required=True, type=str, default='Resnet50')
+    parser.add_argument("--crop", required=False, type=bool, default=False)
+
+    print('Start evaluating .......')
+    start = time.time()
+
+    args = parser.parse_args()
+
+    MAP = compute_mAP(args.feature_extractor, args.crop)
+
+    print(MAP)
+
+    end = time.time()
+    print('Finish in ' + str(end - start) + ' seconds')
+
+if __name__ == '__main__':
+    main()
